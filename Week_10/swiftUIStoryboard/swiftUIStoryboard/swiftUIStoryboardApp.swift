8c523b8b90544ab9ae34cf85294557ca15294159
Johnathan Uptegraph <96315658+J-Uptegraph@users.noreply.github.com>
2023-02-01 18:19:00 -0500

    Add files via upload

---
+++ Week_10/swiftUIStoryboard/swiftUIStoryboard/swiftUIStoryboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIStoryboardApp.swift
+//  swiftUIStoryboard
+//
+//  Created by Johnathan Uptegraph on 11/1/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIStoryboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
